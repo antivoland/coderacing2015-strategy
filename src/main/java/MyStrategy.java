3a26848d6ef76f8f92db62b78856d81d42749aa4
antivoland <antivoland@gmail.com>
2015-11-22 21:09:19 +0300

    experiments

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -6,12 +6,32 @@ import model.World;
 public final class MyStrategy implements Strategy {
     @Override
     public void move(Car self, World world, Game game, Move move) {
-        move.setEnginePower(1.0D);
-        move.setThrowProjectile(true);
-        move.setSpillOil(true);
-
         if (world.getTick() > game.getInitialFreezeDurationTicks()) {
-            move.setUseNitro(true);
+//            move.setUseNitro(true);
+            move.setEnginePower(0.5);
         }
+
+        if (world.getTick() > game.getInitialFreezeDurationTicks() + 380) {
+//            move.setEnginePower(0.0);
+            move.setWheelTurn(1.0);
+//            move.movesetBrake(true);
+        }
+
+        if (world.getTick() > game.getInitialFreezeDurationTicks() + 445) {
+//            move.setEnginePower(1.0);
+            move.setWheelTurn(0.0);
+//            move.movesetBrake(true);
+        }
+
+//        if (world.getTick() > game.getInitialFreezeDurationTicks() + 300) {
+//            move.setWheelTurn(0.0);
+//        }
+
+//        System.out.println(String.format("t: %s, x: %s, y: %s, vx: %s, vy: %s",
+//                world.getTick(), self.getX(), self.getY(), self.getSpeedX(), self.getSpeedY()));
+
+        System.out.println(String.format("%s;%s;%s;%s;%s;%s;%s",
+                world.getTick(), self.getX(), self.getY(), self.getSpeedX(), self.getSpeedY(),
+                self.getWheelTurn(), self.getAngle()));
     }
 }
